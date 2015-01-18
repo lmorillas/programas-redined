d2e551db1917a1b2ac38020aadf035e24531292e
Luis Miguel Morillas <morillas@gmail.com>
2015-01-18 08:56:08 +0100

    Commit inicial

---
+++ redined/redined/items.py
@@ -0,0 +1,36 @@
+# -*- coding: utf-8 -*-
+
+# Define here the models for your scraped items
+#
+# See documentation in:
+# http://doc__scrapy__org/en/latest/topics/items__html
+
+import scrapy
+
+
+class RedinedItem(scrapy.Item):
+    # define the fields for your item here like:
+    # name = scrapy.Field()
+    DCTERMS__extent = scrapy.Field()
+    DCTERMS__abstract = scrapy.Field()
+    citation_date = scrapy.Field()
+    citation_pdf_url = scrapy.Field()
+    DCTERMS__dateAccepted = scrapy.Field()
+    DC__language = scrapy.Field()
+    DC__title = scrapy.Field()
+    DC__type = scrapy.Field()
+    DC__date = scrapy.Field()
+    DC__creator = scrapy.Field()
+    DCTERMS__issued = scrapy.Field()
+    citation_language = scrapy.Field()
+    citation_keywords = scrapy.Field()
+    citation_title = scrapy.Field()
+    DC__description = scrapy.Field()
+    citation_authors = scrapy.Field()
+    citation_abstract_html_url = scrapy.Field()
+    DCTERMS__medium = scrapy.Field()
+    DC__identifier = scrapy.Field()
+    DC__contributor = scrapy.Field()
+    DC__relation = scrapy.Field()
+    DCTERMS__available = scrapy.Field()
+    DC__subject = scrapy.Field()
---
+++ redined/redined/spiders/aragon.py
@@ -0,0 +1,40 @@
+# -*- coding: utf-8 -*-
+import scrapy
+import urlparse
+from redined.items import RedinedItem
+
+BASE = 'http://redined.mecd.gob.es/'
+BASEp = 'http://redined.mecd.gob.es/xmlui/'
+
+class AragonSpider(scrapy.Spider):
+    name = "aragon"
+    allowed_domains = ["redined.mecd.gob.es"]
+    start_urls = (
+        'http://redined.mecd.gob.es/xmlui/discover?scopeFilter=Innovaciones&query=arag%C3%B3n&submit=Ir',
+
+    )
+
+    def parse(self, response):
+        siguientes = response.selector.xpath(u'//a[@class="next-page-link"]/@href').extract()
+        if siguientes:
+            yield scrapy.Request(urlparse.urljoin(BASEp, siguientes[0]))
+        for link in response.selector.xpath(u'//div[@class="artifact-title"]/a/@href').extract():
+            yield scrapy.Request(urlparse.urljoin(BASE, link), callback=self.parse_item)
+
+    def parse_item(self, response):
+        def mangle(x):
+            return x.replace('.', '__')
+        claves = [u'DCTERMS.extent', u'DCTERMS.abstract', u'citation_date',
+        u'citation_pdf_url', u'DCTERMS.dateAccepted', u'DC.language', u'DC.title', u'DC.type',
+        u'DC.date', u'DC.creator', u'DCTERMS.issued', u'citation_language', u'citation_keywords',
+        u'citation_title', u'DC.description', u'citation_authors', u'citation_abstract_html_url',
+        u'DCTERMS.medium', u'DC.identifier', u'DC.contributor', u'DC.relation', u'DCTERMS.available',
+        u'DC.subject']
+
+        exprxp = '//meta[@name="{}"]/@content'
+        item = RedinedItem()
+
+        for k in claves:
+            item[mangle(k)] = response.selector.xpath(exprxp.format(k)).extract()
+        yield item
+
